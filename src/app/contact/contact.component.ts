393161e4b95561a8c49cc2a7a73e91762134dc2e
Nola360 <Adaramolajr@gmail.com>
2023-12-17 23:28:04 -0500

    initial commit

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
 
+  // Contact Form Group
+  contactForm = new FormGroup({
+    senderName: new FormControl('', Validators.required),
+    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    senderMessage: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
+  });
+
+  submitForm() {
+    console.log(this.contactForm.value);
+  }
 }
